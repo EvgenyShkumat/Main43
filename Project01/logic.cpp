bcdaa4295cafd6fc83e27d8c106976281ebb1517
Евгений Шкумат <kotbmasle@gmail.com>
2024-02-24 19:45:19 +0300

    fix find_max_abs_num and  find_min_abs_num. add sum_between_min_max function

--- Project01/logic.cpp
+++ Project01/logic.cpp
@@ -2,34 +2,42 @@
 
 int find_min_abs_num(int* vector, int size) {
 	int min = abs(*vector);
+	int index = 0;
 	
 	for (int i = 1; i < size; i++)
 	{
 		if (min > abs(*(vector + i))) {
 			min = abs(*(vector + i));
+			index = i;
 		}
 	}
 
-	return min;
+	return index;
 }
 
 int find_max_abs_num(int* vector, int size) {
 	int max = *vector;
+	int index = 0;
 
 	for (int i = 1; i < size; i++)
 	{
 		if (max <= *(vector + i)) {
 			max = *(vector + i);
+			index = i;
 		}
 	}
 
-	return max;
+	return index;
 }
 
 int sum_betweem_min_max(int* vector, int size, int min, int max) {
 	int sum = 0;
 
-	for (int i = 0; i < size; i++)
+	if (min > max) {
+		std::swap(min, max);
+	}
+
+	for (int i = ++min; i < max; i++)
 	{
 		sum += *(vector + i);
 	}
